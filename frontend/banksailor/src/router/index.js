2d6c5a47f65d752220d2b2ebe74c2e0c57e76e9f
김승희 <libravickyyy@gmail.com>
2023-11-15 11:22:35 +0900

    [FE] ADD : nav바에 들어가는 view파일 추가

--- frontend/banksailor/src/router/index.js
+++ frontend/banksailor/src/router/index.js
@@ -1,23 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ProductsView from "../views/ProductsView.vue";
+import MapView from "../views/MapView.vue";
+import ExchangeView from "../views/ExchangeView.vue";
+import ArticleView from "../views/ArticleView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/products",
+      name: "products",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      //   component: () => import("../views/AboutView.vue"),
+      component: ProductsView,
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: MapView,
+    },
+    {
+      path: "/exchange",
+      name: "exchange",
+      component: ExchangeView,
+    },
+    {
+      path: "/article",
+      name: "article",
+      component: ArticleView,
+    },
+  ],
+});
 
-export default router
+export default router;
