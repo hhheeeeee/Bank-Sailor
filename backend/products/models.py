1e53f6da9f73ee7a9ef637706f28a9202c43b500
윤동휘 <xorms5712@gmail.com>
2023-11-16 11:57:16 +0900

    [BE] ADD: 예금 및 적금 자료 저장할 테이블 모델 생성

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
-# Create your models here.
+
+# 정기예금 상품 테이블
+class DepositProduct(models.Model):
+    dcls_month = models.CharField(max_length=50)   
+    fin_co_no = models.CharField(max_length=50)                 
+    fin_prdt_cd = models.CharField(max_length=50, unique=True)
+    kor_co_nm = models.CharField(max_length=50)
+    fin_prdt_nm = models.CharField(max_length=100)
+    join_way = models.TextField()
+    mtrt_int = models.TextField()
+    spcl_cnd = models.TextField()
+    join_deny = models.IntegerField()
+    join_member = models.TextField()
+    etc_note = models.TextField()
+    max_limit = models.IntegerField()
+    dcls_strt_day = models.CharField(max_length=50)
+    dcls_end_day = models.CharField(max_length=50)
+    fin_co_subm_day = models.CharField(max_length=50)
+
+
+# 정기예금 옵션 테이블
+class DepositOption(models.Model):
+    # 상품참조할 외래키 설정
+    depositproduct = models.ForeignKey(DepositProduct, on_delete=models.CASCADE)
+    
+    dcls_month = models.CharField(max_length=50)
+    fin_co_no = models.CharField(max_length=50)
+    fin_prdt_cd = models.CharField(max_length=50)
+    intr_rate_type = models.CharField(max_length=50)
+    intr_rate_type_nm = models.CharField(max_length=50)
+    save_trm = models.CharField(max_length=50)
+    intr_rate = models.IntegerField()
+    intr_rate2 = models.IntegerField()
+
+
+# 정기적금 상품 테이블
+class SavingProduct(models.Model):
+    dcls_month = models.CharField(max_length=50)   
+    fin_co_no = models.CharField(max_length=50)                 
+    fin_prdt_cd = models.CharField(max_length=50, unique=True)
+    kor_co_nm = models.CharField(max_length=50)
+    fin_prdt_nm = models.CharField(max_length=100)
+    join_way = models.TextField()
+    mtrt_int = models.TextField()
+    spcl_cnd = models.TextField()
+    join_deny = models.IntegerField()
+    join_member = models.TextField()
+    etc_note = models.TextField()
+    max_limit = models.IntegerField()
+    dcls_strt_day = models.CharField(max_length=50)
+    dcls_end_day = models.CharField(max_length=50)
+    fin_co_subm_day = models.CharField(max_length=50)
+
+
+# 정기적금 옵션 테이블
+class SavingOption(models.Model):
+    # 상품참조할 외래키 설정
+    savingproduct = models.ForeignKey(SavingProduct, on_delete=models.CASCADE)
+    
+    dcls_month = models.CharField(max_length=50)
+    fin_co_no = models.CharField(max_length=50)
+    fin_prdt_cd = models.CharField(max_length=50)
+    intr_rate_type = models.CharField(max_length=50)
+    intr_rate_type_nm = models.CharField(max_length=50)
+    rsrv_type = models.CharField(max_length=50)
+    rsrv_type_nm = models.CharField(max_length=50)
+    save_trm = models.CharField(max_length=50)
+    intr_rate = models.IntegerField()
+    intr_rate2 = models.IntegerField()
--- backend/products/views.py
+++ backend/products/views.py
@@ -3,12 +3,60 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 from django.shortcuts import get_list_or_404, get_object_or_404
+import requests
 
 
+# 정기예금 실행 시 작동
+# 금융감독원의 "정기예금" API 로 부터 자료를 수신받아
+# 데이터베이스에 저장하고,
+# 저장된 데이터베이스를 JSON 형태로 반환
 @api_view(['GET'])
 def deposit_list(request):
-    if request.method == 'POST':
-        pass
+    # API 키
+    API_KEY = '58476eeb85a1feef2bd1156e726ce9ae'
 
+    # 금융감독원 정기예금 JSON 요청 API
+    URL = f'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth={API_KEY}&topFinGrpNo=020000&pageNo=1'
+
+    response = requests.get(URL).json()
+
+    # DepositProduct 에 저장하기
+    for prdt in response.get('result').get('baseList'):
+
+        # DB 레코드에 저장할 각 인스턴스 생성
+        prdt_data = {
+            'dcls_month': prdt.get('dcls_month'),  
+            'fin_co_no': prdt.get('fin_co_no'),               
+            'fin_prdt_cd': prdt.get('fin_prdt_cd'),
+            'kor_co_nm': prdt.get('kor_co_nm'),
+            'fin_prdt_nm': prdt.get('fin_prdt_nm'),
+            'join_way': prdt.get('join_way'),
+            'mtrt_int': prdt.get('mtrt_int'),
+            'spcl_cnd': prdt.get('spcl_cnd'),
+            'join_deny': prdt.get('join_deny'),
+            'join_member': prdt.get('join_member'),
+            'etc_note': prdt.get('etc_note'),
+            'max_limit': prdt.get('max_limit'),
+            'dcls_strt_day': prdt.get('dcls_strt_day'),
+            'dcls_end_day': prdt.get('dcls_end_day'),
+            'fin_co_subm_day': prdt.get('fin_co_subm_day'),
+        }
+
+
+    
+    
+    
+    
+    
+    return Response(response)
+        
+
+@api_view(['GET'])
 def saving_list(request):
-    pass
+    if request.method == 'GET':
+        API_KEY = '58476eeb85a1feef2bd1156e726ce9ae'
+        URL = f'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth={API_KEY}&topFinGrpNo=020000&pageNo=1'
+
+        response = requests.get(URL).json()
+
+        return Response(response)
