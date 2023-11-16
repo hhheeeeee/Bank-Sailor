9645c1e54615fc450667a8ad8b522c181bbb7aff
김현지 <hanguel97@gmail.com>
2023-11-16 09:59:28 +0900

    [FE] ADD : ArticleDetail 및 하위 컴포넌트 생성

--- frontend/banksailor/src/stores/counter.js
+++ frontend/banksailor/src/stores/counter.js
@@ -1,28 +1,61 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
-import axios from "axios";
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
 
-export const useCounterStore = defineStore(
-  "counter",
-  () => {
-    const articles = ref([]);
-    const API_URL = "http://127.0.0.1:8000";
+export const useCounterStore = defineStore('counter', () => {
+  const articles = ref([])  
+  const comments = ref([])
+  const API_URL = 'http://127.0.0.1:8000'
 
-    // DRF에 article 조회 요청을 보내는 action
-    const getArticles = function () {
-      axios({
-        method: "get",
-        url: `${API_URL}/api/v1/articles/`,
+  const getArticles = function () {
+    axios({
+      method: 'get',
+      url: `${API_URL}/articles/articles/`
+    })
+      .then((res) => {
+        articles.value = res.data
       })
-        .then((res) => {
-          // console.log(res)
-          articles.value = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    return { articles, API_URL, getArticles };
-  },
-  { persist: true }
-);
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const createArticle = function () {
+    axios({
+      method: 'post',
+      url: `${API_URL}/articles/articles/`
+    })
+      .then((res) => {
+        console.log(res.data)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+  
+  const getComments = function () {
+    axios({
+      method: 'get',
+      url: `${API_URL}/articles/comments/`,
+    })
+      .then((res) => {
+        comments.value = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const createComments = function () {
+    axios({
+      method: 'post',
+      url: `${API_URL}/articles/articles/<int:article_pk>/comments/`
+    })
+      .then((res) => {
+        console.log(res.data)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return { articles, API_URL, getArticles, createArticle, getComments, }
+}, {persist:true})
