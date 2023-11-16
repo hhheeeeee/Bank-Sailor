09506d7144a320848491f29296791586572e8700
김현지 <hanguel97@gmail.com>
2023-11-16 17:00:12 +0900

    [BE] ADD : 게시글 검색 함수 틀 생성

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('comments/', views.comment_list),
     path('comments/<int:comment_pk>/', views.comment_detail),
     path('articles/<int:article_pk>/comments/', views.comment_create),
+    # path('search/', views.search_article)
 ]
 
