ae5021d94336ea438048c02905a90b83963223f0
김승희 <libravickyyy@gmail.com>
2023-11-17 13:58:12 +0900

    [BE] UPDATE : settings.py AUTH_USER_MODEL 추가

--- backend/banksailor/settings.py
+++ backend/banksailor/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'articles',
     'exchange',
     'products',
+    'accounts',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
@@ -152,3 +153,5 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
