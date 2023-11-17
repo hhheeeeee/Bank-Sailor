638ec778a65223ea2a738d641f5d3362e3da15d3
윤동휘 <xorms5712@gmail.com>
2023-11-17 11:15:53 +0900

    [FE] UPDATE: ProductsDepositDetailView 수정 작업

--- frontend/banksailor/src/views/ProductsDepositDetailView.vue
+++ frontend/banksailor/src/views/ProductsDepositDetailView.vue
@@ -4,15 +4,19 @@
     <a href="#" @click="goDeposit">정기예금</a>  |
     <a href="#" @click="goSaving">적금</a>
   </div>
+  <div>
+    
+  </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 import { onMounted } from 'vue';
 import { useCounterStore } from '@/stores/counter'
 import axios from 'axios'
 
 const router = useRouter()
+const route = useRoute()
 
 const goDeposit = () => {
   router.push({ name: 'deposit' })
@@ -22,6 +26,20 @@ const goSaving = () => {
   router.push({ name: 'saving' })
 }
 
+const store = useCounterStore()
+
+onMounted(() => {
+  axios({
+    method: 'get',
+    url: `${store.API_URL}/products/deposit/${route.params.id}/`
+  })
+  .then((response) => {
+    console.log(response)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
 
 
 
