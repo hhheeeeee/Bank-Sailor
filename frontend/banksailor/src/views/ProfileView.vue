1415634ef4888d54ce51c9952feb5f9bf7676dcc
김승희 <libravickyyy@gmail.com>
2023-11-23 16:10:20 +0900

    [FE] UPDATE : 프로필 메뉴바 초안

--- frontend/banksailor/src/views/ProfileView.vue
+++ frontend/banksailor/src/views/ProfileView.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="container1">
-    <div class="sidebar">
-      <RouterLink :to="{ name: 'editinfo' }" class="link"
-        >기본정보수정</RouterLink
+    <div class="dropdown menubar">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
       >
-      <RouterLink :to="{ name: 'editprofile' }" class="link"
-        >포트폴리오수정</RouterLink
-      >
-      <RouterLink :to="{ name: 'recommend' }" class="link">추천받기</RouterLink>
+        메뉴
+      </button>
+      <ul class="dropdown-menu">
+        <li>
+          <button class="dropdown-item" type="button">
+            <RouterLink :to="{ name: 'editinfo' }" class="link"
+              >기본정보수정</RouterLink
+            >
+          </button>
+        </li>
+        <li>
+          <button class="dropdown-item" type="button">
+            <RouterLink :to="{ name: 'editprofile' }" class="link"
+              >포트폴리오수정</RouterLink
+            >
+          </button>
+        </li>
+        <li>
+          <button class="dropdown-item" type="button">
+            <RouterLink :to="{ name: 'recommend' }" class="link"
+              >추천받기</RouterLink
+            >
+          </button>
+        </li>
+      </ul>
     </div>
+
     <RouterView />
   </div>
 </template>
@@ -31,22 +56,17 @@ import { RouterLink, RouterView } from "vue-router";
   align-items: center;
 }
 
-.sidebar {
-  position: absolute;
-  top: 20%;
-  left: 2%;
-  width: 15%;
-  background-color: rgb(126, 206, 252);
-  opacity: 0.8;
-  border-radius: 30px;
-  display: flex;
-  flex-direction: column;
-}
-
 a {
   text-decoration: none;
   color: rgb(28, 54, 89);
   font-weight: 700;
   margin: 13px auto;
+}
+
+.menubar {
+  position: absolute;
+  top: 13%;
+  left: 9%;
+  width: 20%;
 }
 </style>
