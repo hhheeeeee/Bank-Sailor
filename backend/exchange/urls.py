57ebdc1024722bb00c266ff784d8e2ad8a91494e
김승희 <libravickyyy@gmail.com>
2023-11-24 12:20:15 +0900

    [BE] 환율 사용안하는 url 삭제

--- backend/exchange/urls.py
+++ backend/exchange/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'exchange'
 urlpatterns = [
     path('currency/<str:fromCountry>/<int:price>/<str:st_date>/<str:howlong>/', views.exchange),
-    path('exchange/diff/<str:st_date>/', views.get_exchange_diff),
 ]
 
